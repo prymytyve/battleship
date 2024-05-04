f461a19c3c618a391e9241a4f02032169066c5d5
Jeremy <126707817+prymytyve@users.noreply.github.com>
2024-05-03 23:26:48 -0400

    Refactor tests focusing on the gameboard

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,5 @@
 import Ship from "./mod1_shipClass";
+import Gameboard from "./mod2_gameBoard";
 
 test("Create ship object, and test hit and sunk methods", () => {
   let carrier = new Ship("Carrier", 5);
@@ -16,4 +17,26 @@ test("Create ship object, and test hit and sunk methods", () => {
   carrier.hit();
   carrier.hit();
   expect(carrier.isSunk()).toBeTruthy();
+});
+
+describe("Creating gameboard, and testing interactions with Ship class", () => {
+  const testBoard = new Gameboard();
+  const carrier = new Ship("Carrier", 5);
+  const coordinates = [0, 0];
+
+  // test("placing ship on board", () => {
+  //   testBoard.placeShip(carrier, coordinates);
+  // });
+
+  test("placing ship on board with respects to its length, horizontally", () => {
+    expect(testBoard.gameBoard[0][0]).toContain("Carrier");
+    expect(testBoard.gameBoard[0][1]).toContain("Carrier");
+    expect(testBoard.gameBoard[0][2]).toContain("Carrier");
+    expect(testBoard.gameBoard[0][3]).toContain("Carrier");
+    expect(testBoard.gameBoard[0][4]).toContain("Carrier");
+  });
+
+  test("multiple ships", () => {});
+  test("hit or miss", () => {});
+  test("whether or not all ships have been sunk", () => {});
 });
