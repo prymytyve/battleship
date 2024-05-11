612938f2ee29e28464ff09899ef3e8fcd5e7950e
Jeremy <126707817+prymytyve@users.noreply.github.com>
2024-05-10 23:26:17 -0400

    Generate gameBoard in Dom functionality Creates a table which will act as the battleship gameboard.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./style.css";
+import Player from "./mod3_playerClass";
+
+const gameBoardDisplay = document.querySelector(".gameBoard");
+
+gameBoardDisplay.appendChild(generateGameBoard());
+
+function generateGameBoard() {
+  const board = document.createElement("table");
+  board.classList.add("gameBoard");
+  for (let i = 0; i < 10; i++) {
+    const row = document.createElement("tr");
+    row.classList.add("row");
+    for (let j = 0; j < 10; j++) {
+      const cell = document.createElement("td");
+      cell.classList.add("cell");
+      const coordinates = [i, j];
+      cell.setAttribute("data", coordinates);
+      cell.addEventListener("click", () => {
+        console.log(cell.getAttribute("data"));
+        //add eventfunction here
+      });
+      row.appendChild(cell);
+    }
+    board.appendChild(row);
+  }
+  return board;
+}
