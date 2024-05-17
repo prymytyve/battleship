d3e0f57076bfb10265a1ce37806c65fc0c7c5e27
Jeremy <126707817+prymytyve@users.noreply.github.com>
2024-05-17 02:36:55 -0400

    Refractors ship methods, and adds shipHandler2 ShipHandler2 increases the amount of ship pieces up to threefold.

--- src/mod3_playerClass.js
+++ src/mod3_playerClass.js
@@ -6,21 +6,18 @@ export default class Player {
   constructor(playerName) {
     this._playerName = playerName;
     this._game = new Gameboard();
-    this.ships = shipHandler();
+    this.ships = shipHandler2();
   }
 
   get playerBoard() {
     return this._game.gameBoard;
   }
 
-  placeShip = (coordinates, orientation = "horizontal", errorMsgOn = true) => {
-    if (this.ships.length === 0) return;
+  placeShip = (ship, coordinates, orientation = "horizontal") => {
     try {
-      this._game.placeShip(this.ships[0], coordinates, orientation);
-      this.ships.shift();
+      this._game.placeShip(ship, coordinates, orientation);
     } catch (e) {
-      if (errorMsgOn) console.log(e, this.ships);
-      return;
+      return [ship.shipName, e];
     }
   };
 
@@ -28,7 +25,13 @@ export default class Player {
     while (this.ships.length > 0) {
       let coordinates = rngArray();
       let orientation = rngNum() === 0 ? "horizontal" : "vertical";
-      this.placeShip(coordinates, orientation, false);
+      if (this.ships.length === 0) return;
+      try {
+        this._game.placeShip(this.ships[0], coordinates, orientation);
+        this.ships.shift();
+      } catch (e) {
+        continue;
+      }
     }
   };
 }
@@ -42,4 +45,14 @@ function shipHandler() {
   return [carrier, battleship, cruiser, submarine, destroyer];
 }
 
-//amount & flatmap?
+function shipHandler2(amount = 1) {
+  if (amount === 1) {
+    return shipHandler();
+  } else if (amount === 2) {
+    const board = shipHandler();
+    return board.flatMap((itms) => [itms, itms]);
+  } else if (amount === 3) {
+    const board = shipHandler();
+    return board.flatMap((itms) => [itms, itms, itms]);
+  }
+}
